49c641b6dc9944d88948476823aea66d9bec70d2
InternetWarrior <InternetWarrior@users.noreply.github.com>
2022-12-29 23:33:09 +0600

    beta2

--- vote.js
+++ vote.js
@@ -40,7 +40,6 @@ class VotePrgocess{
     cleanList(){
         for(let i=1; i<=10; i++){
             document.getElementById(i).value = 'false'
-            document.getElementById(i).innerText = `${i}`
         }
        this.voted = []
        this.updateList()
@@ -50,6 +49,10 @@ class VotePrgocess{
 
     copyList(){
         navigator.clipboard.writeText(this.votedText);
+    }
+    
+    copyKill(){
+        navigator.clipboard.writeText('+kill 00 check 00');
     }
 
 
@@ -69,12 +72,12 @@ class VotePrgocess{
                 }
             })
             document.getElementById(event.id).value = 'false'
-            document.getElementById(event.id).innerText = event.id
+            
             this.updateList()
         }else{
             this.voted.push(event.id)
             document.getElementById(event.id).value = 'true'
-            document.getElementById(event.id).innerText =  event.id+' Выставлен!'
+            
             this.updateList()
             
         }
